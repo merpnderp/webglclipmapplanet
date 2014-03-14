3dbcb27f24799aa646bfae3f3af08f73ac9d9e6e
merpnderp <kalebmurphy@gmail.com>
2014-03-13 23:27:38 -0500

    Slowly filling in clipmap code

--- public/app/clipMap.js
+++ public/app/clipMap.js
@@ -2,12 +2,58 @@
 
 var THREE = require('../libs/three.js');
 
-var clipMap = function(){
+var clipMap = function(scale, geo){
+    this.geo = geo;
+    this.meshes = [];
+    // Create center layer first
+    // +---+---+
+    // | O | O |
+    // +---+---+
+    // | O | O |
+    // +---+---+
+    this.createTile(-scale, -scale, scale, Edge.NONE);
+    this.createTile(-scale, 0, scale, Edge.NONE);
+    this.createTile(0, 0, scale, Edge.NONE);
+    this.createTile(0, -scale, scale, Edge.NONE);
+
+    // Create "quadtree" of tiles, with smallest in center
+    // Each added layer consists of the following tiles (marked 'A'), with the tiles
+    // in the middle being created in previous layers
+    // +---+---+---+---+
+    // | A | A | A | A |
+    // +---+---+---+---+
+    // | A |   |   | A |
+    // +---+---+---+---+
+    // | A |   |   | A |
+    // +---+---+---+---+
+    // | A | A | A | A |
+    // +---+---+---+---+
+
+    this.createTile( -2 * scale, -2 * scale, scale, Edge.BOTTOM | Edge.LEFT );
+    this.createTile( -2 * scale, -scale, scale, Edge.LEFT );
+    this.createTile( -2 * scale, 0, scale, Edge.LEFT );
+    this.createTile( -2 * scale, scale, scale, Edge.TOP | Edge.LEFT );
+
+    this.createTile( -scale, -2 * scale, scale, Edge.BOTTOM );
+    // 2 tiles 'missing' here are in previous layer
+    this.createTile( -scale, scale, scale, Edge.TOP );
+
+    this.createTile( 0, -2 * scale, scale, Edge.BOTTOM );
+    // 2 tiles 'missing' here are in previous layer
+    this.createTile( 0, scale, scale, Edge.TOP );
+
+    this.createTile( scale, -2 * scale, scale, Edge.BOTTOM | Edge.RIGHT );
+    this.createTile( scale, -scale, scale, Edge.RIGHT );
+    this.createTile( scale, 0, scale, Edge.RIGHT );
+    this.createTile( scale, scale, scale, Edge.TOP | Edge.RIGHT );
 
 }
 
 clipMap.prototype = {
-
+    createTile: function(x, y, scale, edgeMorphy){
+        var terrainMaterial = new THREE.MeshBasicMaterial();
+        this.meshes.push(new THREE.Mesh(this.geo, terrainMaterial));
+    }
 };
 
 module.exports = clipMap;
--- public/app/sphericalTerrain.js
+++ public/app/sphericalTerrain.js
@@ -1,15 +1,32 @@
 var THREE = require('../libs/three.js');
+var ClipMap = require('./clipMap');
 
+var Edge = {
+    NONE: 0,
+    TOP: 1,
+    LEFT: 2,
+    BOTTOM: 4,
+    RIGHT: 8
+};
 
 var terrain = function (resolution, radius, clipMapCount) {
 
     THREE.Object3D.call( this );
 
+    this.radius = radius;
+    this.resolution = resolution;
+    this.clipMapCount = clipMapCount;
+
     this.geometry = new THREE.PlaneGeometry(1, 1, resolution, resolution);
 
     this.position = new THREE.Vector3(0, 0, 0);
+    this.clipMaps = [];
+
+    this.createClipMaps();
 
 }
+
+terrain.prototype = Object.create( THREE.Object3D.prototype );
 
 terrain.prototype = {
 
@@ -17,9 +34,15 @@ terrain.prototype = {
 
     },
 
+    createClipMaps: function(){
+        var scale = this.radius;
+        for(var i = 0; i < this.clipMapCount; i++){
 
-    createClipMap: function(x, y, scale, edgeMorph){
+            this.clipMap[i] = new ClipMap(scale, this.geometry);
 
+            scale /= 2;
+
+        }
     }
 
 };
